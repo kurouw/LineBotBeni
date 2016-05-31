3984454cd136d9daf39b77c045bc8c84ac114542
kurouw <github0220@gmail.com>
2016-05-31 14:24:28 +0900

    mongo2

--- users.rb
+++ users.rb
@@ -3,10 +3,10 @@ class User
   field :toId, type: String
   field :pref, type: String
   field :shopName, type: String
-=begin
+
   validates :toId, presence: true
   validates :toId, unqueness: true
   validates :pref, presence: true
   validates :shopName, presence: true
-=end
+
 end
